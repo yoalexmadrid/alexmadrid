11742aa5d9c56e42627859089e4fbaf4673dfb32
yoalexmadrid <72267280+yoalexmadrid@users.noreply.github.com>
2020-10-02 09:12:05 -0600

    parcial2

---
+++ parcial 2/notas.js
@@ -0,0 +1,11 @@
+function promedio(){
+    var taller_uno = document.getElementById("taller_uno").value;
+    var taller_dos = document.getElementById("taller_dos").value;
+    var parcial = document.getElementById("parcial").value;
+    var confirmacion = document.getElementById("confirmacion").checked;
+
+
+  
+
+
+}
